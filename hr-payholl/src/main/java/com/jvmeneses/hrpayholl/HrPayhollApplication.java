650169eb458a7ec79cc2918e2c89e1b241456977
Joao <joaovic2012@hotmail.com>
2023-04-26 22:53:27 -0300

    hr-payroll created

---
+++ hr-payholl/src/main/java/com/jvmeneses/hrpayholl/HrPayhollApplication.java
@@ -0,0 +1,13 @@
+package com.jvmeneses.hrpayholl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrPayhollApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrPayhollApplication.class, args);
+	}
+
+}
---
+++ hr-payholl/src/test/java/com/jvmeneses/hrpayholl/HrPayhollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jvmeneses.hrpayholl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrPayhollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
