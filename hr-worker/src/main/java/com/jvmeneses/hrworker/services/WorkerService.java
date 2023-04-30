f24618387602b4ea61ea9614dafa5c80c3ba7abe
Joao <joaovic2012@hotmail.com>
2023-04-30 20:56:32 -0300

    Ribbon load balacing

--- hr-worker/src/main/java/com/jvmeneses/hrworker/services/WorkerService.java
+++ hr-worker/src/main/java/com/jvmeneses/hrworker/services/WorkerService.java
@@ -3,7 +3,10 @@ package com.jvmeneses.hrworker.services;
 import com.jvmeneses.hrworker.dto.WorkerDTO;
 import com.jvmeneses.hrworker.entities.Worker;
 import com.jvmeneses.hrworker.repositories.WorkerRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -12,6 +15,11 @@ import javax.transaction.Transactional;
 
 @Service
 public class WorkerService {
+
+    private static Logger logger = LoggerFactory.getLogger(WorkerService.class);
+
+    @Autowired
+    private Environment env;
 
     @Autowired
     private WorkerRepository workerRepository;
@@ -24,6 +32,9 @@ public class WorkerService {
 
     @Transactional
     public WorkerDTO findById(Long id){
+
+        logger.info("PORT = " + env.getProperty("local.server.port"));
+
         Worker entity = workerRepository.findById(id).get();
         return new WorkerDTO(entity);
     }
