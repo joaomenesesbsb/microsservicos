e39342a1cf949c55811c68ab801fd0891a451fbc
Joao <joaovic2012@hotmail.com>
2023-04-30 20:16:20 -0300

    Downgrade, Feing Client

--- hr-payholl/src/main/java/com/jvmeneses/hrpayholl/services/PaymentService.java
+++ hr-payholl/src/main/java/com/jvmeneses/hrpayholl/services/PaymentService.java
@@ -2,28 +2,19 @@ package com.jvmeneses.hrpayholl.services;
 
 import com.jvmeneses.hrpayholl.entities.Payment;
 import com.jvmeneses.hrpayholl.entities.Worker;
+import com.jvmeneses.hrpayholl.feignClients.WorkerFeignClient;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Service
 public class PaymentService {
 
-    @Value("${hr-worker.host}")
-    private String workerHost;
-
     @Autowired
-    private RestTemplate restTemplate;
+    private WorkerFeignClient workerFeignClient;
 
     public Payment getPayment(long workerId , int days){
-        Map<String, String> uriVariables = new HashMap<>();
-        uriVariables.put("id", ""+workerId);
 
-        Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
+        Worker worker = workerFeignClient.findById(workerId).getBody();
         return new Payment(worker.getName(), worker.getDailyIncome(), days);
     }
 }
