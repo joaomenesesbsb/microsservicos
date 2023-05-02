73141a7aa12600dc15d74d4c04d2b4b45c2be4ce
Joao <joaovic2012@hotmail.com>
2023-05-02 20:53:58 -0300

    Configuring Zull api Gateway

--- hr-payroll/src/main/java/com/jvmeneses/hrpayholl/HrPayrollApplication.java
+++ hr-payroll/src/main/java/com/jvmeneses/hrpayholl/HrPayrollApplication.java
@@ -4,17 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableCircuitBreaker
 @EnableEurekaClient
 @EnableFeignClients
 @SpringBootApplication
-public class HrPayhollApplication {
+public class HrPayrollApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HrPayhollApplication.class, args);
+		SpringApplication.run(HrPayrollApplication.class, args);
 	}
 
 }
